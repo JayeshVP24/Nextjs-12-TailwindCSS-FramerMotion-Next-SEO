ba911b4881f5f2b21f23faed9d74a4cf858bcd4b
Mr-Jayesh <jayeshpotlabattini@gmail.com>
2021-11-30 14:33:22 +0530

    next js 12 update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,40 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-
+import "../styles/globals.css";
+import { DefaultSeo } from "next-seo";
+import SEO from "../next-seo.config";
+import { AppProps } from "next/app";
+import Head from "next/head";
+import { AnimatePresence, domAnimation, LazyMotion } from "framer-motion";
+// import { IKContext} from 'imagekitio-react';
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    // const PUBLIC_KEY = "public_DdnXL0ip5CRZeQdeSI4QrmwIetA=";
+    // const URL_ENDPOINT = "https://ik.imagekit.io/elightlabs/Annamaya";
+
+    return (
+        <>
+            <Head>
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
+                <meta
+                    httpEquiv="Content-Type"
+                    content="text/html; charset=utf-8 "
+                />
+            </Head>
+            {/* <IKContext
+            publicKey={PUBLIC_KEY}
+            urlEndpoint={URL_ENDPOINT}>             */}
+            <div>
+                <DefaultSeo {...SEO} />
+                <LazyMotion features={domAnimation}>
+                    <AnimatePresence exitBeforeEnter>
+                        <Component {...pageProps} />
+                    </AnimatePresence>
+                </LazyMotion>
+            </div>
+            {/* </IKContext> */}
+        </>
+    );
 }
 
-export default MyApp
+export default MyApp;
